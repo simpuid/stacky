4f15de1ffe6319d2e5843dd689ea8a30b8e890b2
Simpu <utkarsh.email@yahoo.com>
2019-05-24 16:38:14 +0530

    Added  special output stack

---
+++ include/special.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory.hpp>
+
+class Output : public Stack
+{
+    void push(memseg data);
+};
---
+++ src/special.cpp
@@ -0,0 +1,9 @@
+#include <special.hpp>
+#include <iostream>
+
+using namespace std;
+
+void Output::push(memseg data)
+{
+    cout << data;
+}
