c541b4938baee6ae8cef0c76d2a7094736070cad
Simpu <utkarsh.email@yahoo.com>
2019-05-24 16:35:14 +0530

    Added the graph data structure

--- include/graph.hpp
+++ include/graph.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <memory.hpp>
 using namespace std;
 
 enum class Operation
@@ -20,24 +21,23 @@ class Operator : public Statement
 {
 public:
     vector<Operation> operations;
-    string target;
-    Operator(vector<Operation> operations, string target);
+    unique_ptr<Stack> &target;
+    Operator(vector<Operation> operations, unique_ptr<Stack> &target);
     void accept(Visitor &v);
 };
-
 class SetStack : public Statement
 {
 public:
-    string name;
-    SetStack(string name);
+    unique_ptr<Stack> &stack;
+    SetStack(unique_ptr<Stack> &stack);
     void accept(Visitor &v);
 };
 
 class SetNumber : public Statement
 {
 public:
-    string number;
-    SetNumber(string number);
+    memseg number;
+    SetNumber(memseg number);
     void accept(Visitor &v);
 };
 class ZeroBlock : public Statement
@@ -52,26 +52,3 @@ public:
     vector<unique_ptr<Statement>> statements;
     void accept(Visitor &v);
 };
-
-class Visitor
-{
-public:
-    virtual void visit(Statement &target) = 0;
-    virtual void visit(SetStack &target) = 0;
-    virtual void visit(SetNumber &target) = 0;
-    virtual void visit(ZeroBlock &target) = 0;
-    virtual void visit(EmptyBlock &target) = 0;
-    virtual void visit(Operator &target) = 0;
-};
-
-class Printer : public Visitor
-{
-    void visit(Statement &target);
-    void visit(SetStack &target);
-    void visit(SetNumber &target);
-    void visit(ZeroBlock &target);
-    void visit(EmptyBlock &target);
-    void visit(Operator &target);
-};
-
-void accept(Statement *statement, Visitor &visitor);
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,12 +1,13 @@
 #include <graph.hpp>
-#include <iostream>
+#include <visitor.hpp>
 
-Operator::Operator(vector<Operation> operations, string target) : operations(operations), target(target) {}
-SetStack::SetStack(string name) : name(name){};
-SetNumber::SetNumber(string number) : number(number) {}
+Operator::Operator(vector<Operation> operations, unique_ptr<Stack>& target) : operations(operations), target(target) {}
+SetStack::SetStack(unique_ptr<Stack> &stack) : stack(stack){};
+SetNumber::SetNumber(memseg number) : number(number) {}
 
 void Statement::accept(Visitor &v)
 {
+    v.visit(*this);
 }
 void SetStack::accept(Visitor &v)
 {
@@ -27,67 +28,4 @@ void EmptyBlock::accept(Visitor &v)
 void Operator::accept(Visitor &v)
 {
     v.visit(*this);
-}
-void accept(Statement *statement, Visitor &visitor)
-{
-    if (dynamic_cast<Operator *>(statement) != nullptr)
-    {
-        dynamic_cast<Operator *>(statement)->accept(visitor);
-    }
-    else if (dynamic_cast<SetStack *>(statement) != nullptr)
-    {
-        dynamic_cast<SetStack *>(statement)->accept(visitor);
-    }
-    else if (dynamic_cast<SetNumber *>(statement) != nullptr)
-    {
-        dynamic_cast<SetNumber *>(statement)->accept(visitor);
-    }
-    else if (dynamic_cast<ZeroBlock *>(statement) != nullptr)
-    {
-        dynamic_cast<ZeroBlock *>(statement)->accept(visitor);
-    }
-    else if (dynamic_cast<EmptyBlock *>(statement) != nullptr)
-    {
-        dynamic_cast<EmptyBlock *>(statement)->accept(visitor);
-    }
-}
-
-void Printer::visit(SetStack &target)
-{
-    cout << "stack(" << target.name << ")";
-}
-void Printer::visit(SetNumber &target)
-{
-    cout << "number(" << target.number << ')';
-}
-void Printer::visit(ZeroBlock &target)
-{
-    cout << "[";
-    for (auto &t : target.statements)
-    {
-        accept(t.get(), *this);
-    }
-    cout << "]";
-}
-void Printer::visit(EmptyBlock &target)
-{
-    cout << "{";
-    for (auto &t : target.statements)
-    {
-        accept(t.get(), *this);
-    }
-    cout << "}";
-}
-void Printer::visit(Statement &target)
-{
-    cout << "statement";
-}
-void Printer::visit(Operator &target)
-{
-    cout << "operator(";
-    for (auto op : target.operations)
-    {
-        cout << (op == Operation::Move ? '>' : '+'); 
-    }
-    cout << ", "<<target.target<<" )";
 }
